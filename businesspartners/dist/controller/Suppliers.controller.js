27a24ef56f165e725991c0d092ae74e5a71ac30b
Guido Jacobs <guido.jacobs@achmea.nl>
2023-09-13 12:46:23 +0000

    Initial Commit @9/13/2023, 12:46:23 PM

---
+++ businesspartners/dist/controller/Suppliers.controller.js
@@ -0,0 +1,2 @@
+sap.ui.define(["sap/ui/core/mvc/Controller"],function(n){"use strict";return n.extend("ns.businesspartners.controller.Suppliers",{onInit:function(){}})});
+//# sourceMappingURL=Suppliers.controller.js.map
